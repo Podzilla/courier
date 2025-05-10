2f515c4d7cca7ca94e47eb9eaf55f6f8db156493
omar mohammed <omarb2515@gmail.com>
2025-05-10 18:33:58 +0300

    add courier mapper

--- src/main/java/com/podzilla/courier/mappers/CourierMapper.java
+++ src/main/java/com/podzilla/courier/mappers/CourierMapper.java
@@ -9,16 +9,21 @@ public class CourierMapper {
 
     public static Courier toEntity(CreateCourierRequestDto dto) {
         Courier courier = new Courier();
+        courier.setName(dto.getName());
+        courier.setMobileNo(dto.getMobileNo());
         return courier;
     }
+
     public static Courier toEntity(UpdateCourierRequestDto dto) {
         Courier courier = new Courier();
+        courier.setId(dto.getId());
+        if (dto.getName() != null) courier.setName(dto.getName());
+        if (dto.getMobileNo() != null) courier.setMobileNo(dto.getMobileNo());
+        if (dto.getStatus() != null) courier.setStatus(dto.getStatus());
         return courier;
     }
 
     public static CourierResponseDto toCreateResponseDto(Courier courier) {
-        return new CourierResponseDto();
+        return new CourierResponseDto(courier.getId(), courier.getName(), courier.getMobileNo());
     }
-
-
 }
