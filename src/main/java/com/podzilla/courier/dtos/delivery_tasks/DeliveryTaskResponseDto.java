740ab31eedfe6449235b5f7629963276b939c080
omar mohammed <omarb2515@gmail.com>
2025-05-17 17:45:11 +0300

    update files to pass check-style

--- src/main/java/com/podzilla/courier/dtos/delivery_tasks/DeliveryTaskResponseDto.java
+++ src/main/java/com/podzilla/courier/dtos/delivery_tasks/DeliveryTaskResponseDto.java
@@ -6,7 +6,6 @@ import com.podzilla.courier.models.DeliveryStatus;
 import java.math.BigDecimal;
 
 public record DeliveryTaskResponseDto(String id, String orderId, String courierId, BigDecimal price,
-                                      DeliveryStatus status,
-                                      Double orderLatitude, Double orderLongitude, Double courierLatitude,
-                                      Double courierLongitude) {
+                                      DeliveryStatus status, Double orderLatitude, Double orderLongitude,
+                                      Double courierLatitude, Double courierLongitude) {
 }
